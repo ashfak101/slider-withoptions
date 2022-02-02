39ff78915382542db3837af2a9851a74056376e0
Staff Asia <staffasia@Staffs-iMac-2.local>
2022-02-02 15:32:11 +0600

    Dots and Navigation implemented

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
 const ashfakSlider = (options) => {
   let interval = options.interval,
     autoplay = options.autoplay,
-    nav = options.nav;
+    nav = options.nav,
+    item = options.item,
+    dots = options.dots;
+
+  // Dom
   const container = document.getElementById("container");
   const slider = document.querySelector(".slider");
   const slide = document.querySelectorAll(".slide");
+  //   const slides = document.querySelector(".slide");
+
   //   slider.style.display = "none";
   console.log(container);
   console.log(slider.offsetWidth);
+  let items = slider.offsetWidth / item;
+  let count = slide.length / item;
   let i = 0;
+  //   slide.style.minWidth = slider.offsetWidth + "px";
   // /Navigation enable
+  slide.forEach((singleSlide) => {
+    singleSlide.style.minWidth = items + "px";
+  });
+
+  //   ---------Dots---------------------
+  if (dots) {
+    const ul = document.createElement("ul");
+    ul.classList.add("dots");
+    let dotCount = Math.ceil(slide.length / item);
+    while (dotCount >= 1) {
+      const li = document.createElement("li");
+
+      ul.appendChild(li);
+
+      dotCount--;
+    }
+    container.appendChild(ul);
+
+    const dotsClick = document.querySelectorAll(".dots li");
+    const firstChild = document.querySelector(".dots li");
+    firstChild.classList.add("active");
+    //   Adding EventListener on Dots-----------
+    dotsClick.forEach((dot, index) => {
+      dot.addEventListener("click", () => {
+        i = index;
+        document.querySelector(".dots .active").classList.remove("active");
+        dot.classList.add("active");
+        slider.style.left = -slider.offsetWidth * i + "px";
+      });
+    });
+  }
+
+  let bullets = document.getElementsByTagName("li");
+  console.log(bullets);
+  //   ---------------------------
   if (nav) {
+    //   Navigation
     const div = document.createElement("div");
     div.innerHTML = `
         <span class="prev">prev</span>
@@ -22,7 +67,11 @@ const ashfakSlider = (options) => {
     // next navigation
     next.addEventListener("click", () => {
       i = i < slide.length ? i + 1 : 0;
+
       slider.style.left = -slider.offsetWidth * i + "px";
+      document.querySelector(".dots li").classList.remove("active");
+
+      bullets.children[i + 1].classList.add("active");
     });
     //   console.log(next);
     //   prev navigation
@@ -31,11 +80,10 @@ const ashfakSlider = (options) => {
       slider.style.left = -slider.offsetWidth * i + "px";
     });
   }
-  //   Navigation
-
+  //
   if (autoplay === true) {
     setInterval(() => {
-      i = i < slide.length ? i + 1 : 0;
+      i = i < count ? i + 1 : 0;
       slider.style.left = -slider.offsetWidth * i + "px";
     }, interval);
   }
