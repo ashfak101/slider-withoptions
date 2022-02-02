09db56ee1f6711ad46827102c9c4a0b2b12e7a8b
Staff Asia <staffasia@Staffs-iMac-2.local>
2022-02-02 13:00:04 +0600

    New Functionality Added

--- app.js
+++ app.js
@@ -1,67 +1,42 @@
-console.log("HEllo");
-
-const carousel = (options) => {
-  let element = document.getElementById(options.element || "carousel"),
-    intervel = options.intervel || 3000,
-    nextSlide = options.nextSlide || "next",
-    prevSlide = options.prevSlide || "prev",
-    count = element.querySelectorAll(" .slide").length,
-    current = 0,
-    cycle = null,
-    crslClass = 'js-Carousel',
-
-  if (count > 1) {
-    display();
+const ashfakSlider = (options) => {
+  let interval = options.interval,
+    autoplay = options.autoplay,
+    nav = options.nav;
+  const container = document.getElementById("container");
+  const slider = document.querySelector(".slider");
+  const slide = document.querySelectorAll(".slide");
+  //   slider.style.display = "none";
+  console.log(container);
+  console.log(slider.offsetWidth);
+  let i = 0;
+  // /Navigation enable
+  if (nav) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <span class="prev">prev</span>
+          <span class="next">next</span>
+      `;
+    container.appendChild(div);
+    const next = document.querySelector(".next");
+    const prev = document.querySelector(".prev");
+    // next navigation
+    next.addEventListener("click", () => {
+      i = i < slide.length ? i + 1 : 0;
+      slider.style.left = -slider.offsetWidth * i + "px";
+    });
+    //   console.log(next);
+    //   prev navigation
+    prev.addEventListener("click", () => {
+      i = i > 0 ? i - 1 : 0;
+      slider.style.left = -slider.offsetWidth * i + "px";
+    });
   }
+  //   Navigation
 
-  const display = () => {
-    let action = {
-      dots: function () {
-        return showDots();
-      },
-      arrows: function () {
-        return showArrows();
-      },
-      autoplay: function () {
-        return showAutoplay();
-      },
-      infinite: function () {
-        return moveItem(count - 1, -element.offsetWidth + "px", "afterBegin");
-      },
-      initial: function () {
-        let initial = 0 || options.initial >= count ? 0 : options.initial;
-        return show(initial);
-      },
-    };
-    for (let key in action) {
-      if (options.hasOwnProperty(key) && options[key]) {
-        action[key]();
-      }
-    }
-  };
-
-  const moveItem=(index,marginLeft,position)=>{
-      let itemToMove=element.querySelectorAll('.'+crslClass+'> div .slide')[index];
-      itemToMove.style.marginLeft=marginLeft;
-
-      element.querySelector('.'+crslClass+'> div').removeChild(itemToMove);
-      element.querySelector('.'+crslClass+'> div').insertAdjacentElement(position,itemToMove.outerHTML);
+  if (autoplay === true) {
+    setInterval(() => {
+      i = i < slide.length ? i + 1 : 0;
+      slider.style.left = -slider.offsetWidth * i + "px";
+    }, interval);
   }
-
- const showDots = () => {
-     let dotContainer = document.createElement("ul")   ;
-        dotContainer.classList.add("dots");
-        dotContainer.addEventListener('click',scrollToImage.bind(this));
-
-        for(let i=0; i<count; i++){
-            let dot=document.createElement("li");
-            dot.setAttribute("data-index",i);
-            dotContainer.appendChild(dot);
-        }
-        element.appendChild(dotContainer);
-        currentDot();
- }
-
-
-
 };
